de82eb2aa79dc14074ecd41c49db1f061492bd9a
oomusou <oomusou@gmail.com>
2018-08-20 21:41:52 +0800

    use binding

---
+++ ConsoleApp/Program.fs
@@ -0,0 +1,8 @@
+﻿open System.IO
+
+let readFromFile (fileName: string) = 
+    use streamReader = new StreamReader(fileName)
+    streamReader.ReadToEnd()
+    
+readFromFile "TestFile.txt"
+|> printf "%A"   
